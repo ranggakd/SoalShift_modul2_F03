493027b4afe191eb2562ac26ff313eb074320954
ranggakd <ranggakd@users.noreply.github.com>
2019-03-13 16:01:18 +0700

    Update soal5b.cpp

--- soal5/soal5b.cpp
+++ soal5/soal5b.cpp
@@ -2,7 +2,7 @@
 #include<unistd.h>
 
 int main(){
-    char* hapus[]={(char*)"killall", (char*)"soal5c", NULL};
+    char* hapus[]={(char*)"killall", (char*)"soal5a", NULL};
     execv("/usr/bin/killall", hapus);
     return 0;
 }
